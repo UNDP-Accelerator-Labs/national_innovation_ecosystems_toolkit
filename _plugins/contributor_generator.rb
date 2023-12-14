1af24296b7f00d8c7dc5f674d5e740b68f549004
Adedapo Aderemi <adedapopaul@yahoo.com>
2023-12-14 12:51:51 +0000

    Update toolkit template

--- _plugins/contributor_generator.rb
+++ _plugins/contributor_generator.rb
@@ -1,31 +1,32 @@
 module Jekyll
-    class ContributorPageGenerator < Generator
-      priority :lowest
-  
-      def generate(site)
-        contributors = site.data["contributors"]
-  
-        contributors.each do |contributor_id, contributor_data|
-          contributor = ContributorPage.new(site, site.source, "_layouts", contributor_id, contributor_data)
-          contributor.render(site.layouts, site.site_payload)
-          contributor.write(site.dest)
-          site.pages << contributor
-        end
-      end
-    end
-  
-    class ContributorPage < Page
-      def initialize(site, base, dir, contributor_id, contributor_data)
-        @site = site
-        @base = base
-        @dir  = "/contributors"
-        @name = "#{contributor_id}.html"
-  
-        self.process(@name)
-        self.read_yaml(File.join(base, "_layouts"), "contributor.html")
-        self.data["title"] = contributor_data["name"]
-        self.data["contributor"] = contributor_data
+  class ContributorPageGenerator < Generator
+    priority :lowest
+
+    def generate(site)
+      contributors = site.data["contributors"]
+
+      contributors.each do |contributor_id, contributor_data|
+        contributor = ContributorPage.new(site, site.source, "_layouts", contributor_id, contributor_data)
+        contributor.render(site.layouts, site.site_payload)
+        contributor.write(site.dest)
+        site.pages << contributor
       end
     end
   end
-  
+
+  class ContributorPage < Page
+    def initialize(site, base, dir, contributor_id, contributor_data)
+      @site = site
+      @base = base
+      @dir  = "/contributors"
+      @name = "#{contributor_id}.html"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, "_layouts"), "contributor.html")
+      self.data["title"] = contributor_data["name"]
+      self.data["contributor"] = contributor_data
+      self.data["contributor"]["id"] = contributor_id
+      self.data["contributor"]["href"] = "/contributors" + "/#{contributor_id}.html"
+    end
+  end
+end
